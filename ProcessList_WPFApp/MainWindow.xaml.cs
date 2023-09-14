c96370b5899d4db2182914ac4e888f9f23b28d30
tsouris <nikolastsouris16@gmail.com>
2023-09-14 16:00:50 +0300

    Finished Task 4

--- ProcessList_WPFApp/MainWindow.xaml.cs
+++ ProcessList_WPFApp/MainWindow.xaml.cs
@@ -1,15 +1,69 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System;
+using System.Windows;
 
 namespace ProcessList_WPFApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    //TASK 4
+    //Develop a WPF application that allows the user to launch other programs.
+    //The user can start:
+    //Notepad.
+    //Calculator.
+    //Paint.
+    //Their own, another application
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void LaunchNotepad_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Process.Start("notepad.exe");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error launching Notepad: " + ex.Message);
+            }
+        }
+
+        private void LaunchCalculator_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Process.Start("calc.exe");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error launching Calculator: " + ex.Message);
+            }
+        }
+
+        private void LaunchPaint_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Process.Start("mspaint.exe");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error launching Paint: " + ex.Message);
+            }
+        }
+
+        private void LaunchSpotify_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Process.Start("C:\\Users\\User\\AppData\\Roaming\\Spotify\\Spotify.exe");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error launching custom program: " + ex.Message);
+            }
         }
     }
 }
