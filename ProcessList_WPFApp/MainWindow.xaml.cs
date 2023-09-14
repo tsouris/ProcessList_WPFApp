a58aa446267d505a05bcb0c9e0916c9d1acf2879
tsouris <nikolastsouris16@gmail.com>
2023-09-14 18:16:58 +0300

    Finished hw 1

--- ProcessList_WPFApp/MainWindow.xaml.cs
+++ ProcessList_WPFApp/MainWindow.xaml.cs
@@ -1,15 +1,30 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Windows;
 
 namespace ProcessList_WPFApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    //TASK 1
+    //Develop an application that can launch a child process and wait for its completion.
+    //When the child process is finished, the parent application should display the exit code.
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void LaunchProcess_Click(object sender, RoutedEventArgs e)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = "calc.exe";
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.Start();
+
+            process.WaitForExit();
+
+            int exitCode = process.ExitCode;
+            exitCodeLabel.Content = "Exit Code: " + exitCode.ToString();
         }
     }
 }
